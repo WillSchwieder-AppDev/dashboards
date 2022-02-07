ecc5dad45ba5f9ed521c6fbfb07745c2cbd2609d
WillSchwieder <wschwied@chicagobooth.edu>
2022-02-07 05:29:18 +0000

    Completed the assignment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,49 @@
 class ApplicationController < ActionController::Base
+  def dashboard
+    render("/dashboard.html.erb")
+  end
+
+  def forex
+    # Parameters: {"user_street_address"=>"Merchandise Mart, Chicago"}
+
+    @forex_url = "https://api.exchangerate.host/symbols"
+
+    @raw_json_string = open(@forex_url).read
+
+    @parsed_json = JSON.parse(@raw_json_string)
+
+    @results_array = @parsed_json.fetch("symbols").keys
+
+    render("forex_templates/forex.html.erb")
+  end
+
+  def forex_results
+    @curr_result = params.fetch("country")
+
+    @forex_url = "https://api.exchangerate.host/symbols"
+
+    @raw_json_string = open(@forex_url).read
+
+    @parsed_json = JSON.parse(@raw_json_string)
+
+    @results_array = @parsed_json.fetch("symbols").keys
+  
+    render({:template => "forex_templates/forex_results.html.erb"})
+  end
+
+  def forex_results2
+    @curr_result_1 = params.fetch("country1")
+    @curr_result_2 = params.fetch("country2")
+
+    @forex_url = "https://api.exchangerate.host/convert?from=" + @curr_result_1 + "&to=" + @curr_result_2
+
+    @raw_json_string = open(@forex_url).read
+
+    @parsed_json = JSON.parse(@raw_json_string)
+
+    @results = @parsed_json.fetch("result")
+  
+    render({:template => "forex_templates/forex_results2.html.erb"})
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get("/dashboard", { :controller => "application", :action => "dashboard" })
+  get("/forex", { :controller => "application", :action => "forex" })
+  get("/forex/:country", { :controller => "application", :action => "forex_results" })
+  get("/forex/:country1/:country2", { :controller => "application", :action => "forex_results2" })
 end
